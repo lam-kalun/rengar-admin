95b744163fe7ebc7203b5f93d8c84f01731ec751
ashuicoder <1249735120@qq.com>
2025-02-27 14:31:36 +0800

    ✨ feat: icon

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,7 +3,7 @@
     <div class="w-full flex items-center justify-center gap-x-4 text-4xl p-2 mt-4">
       <div class="i-vscode-icons:file-type-light-pnpm" />
       <div class="i-local-logo"></div>
-      <Icon icon="mdi-light:home" />
+      <Icon icon="i-mdi-light:home" />
     </div>
     <div v-for="item in 100" :key="item">{{ item }}</div>
   </NCard>
