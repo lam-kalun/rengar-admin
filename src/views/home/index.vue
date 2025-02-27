6ad08f322e6df49157d79bed2889aa65fd9d92b6
ashuicoder <1249735120@qq.com>
2025-02-27 14:27:45 +0800

    ✨ feat: icon测试

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,11 +3,14 @@
     <div class="w-full flex items-center justify-center gap-x-4 text-4xl p-2 mt-4">
       <div class="i-vscode-icons:file-type-light-pnpm" />
       <div class="i-local-logo"></div>
+      <Icon icon="mdi-light:home" />
     </div>
     <div v-for="item in 100" :key="item">{{ item }}</div>
   </NCard>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+</script>
 
 <style scoped></style>
