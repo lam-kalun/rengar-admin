b03c959ca76b16392448f54440b0932154632a62
ashuicoder <1249735120@qq.com>
2025-03-05 13:09:28 +0800

    ✨ feat: lint

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,6 @@
 <template>
   <NCard>
-    <div class="i-mdi-ab-testing text-2xl text-red"></div>
+    <div class="text-2xl i-mdi-ab-testing text-red"></div>
     <div class="i-local-logo" style="font-size: 40px; color: #f40"></div>
     <SvgIcon icon="i-local-logo" class="text-2xl text-red"></SvgIcon>
     <div class="text-primary">222</div>
